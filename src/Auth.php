795d6d8a6fe6bb310825d18be8ee216a39ef8c4c
Christopher CHEN <chenshaoming@appgame.com>
2016-11-23 21:47:46 +0800

    fix: config conflicts with auth

--- src/Auth.php
+++ src/Auth.php
@@ -8,18 +8,44 @@ use Phwoolcon\Admin\Auth\Adapter\Generic;
 use Phwoolcon\Admin\Model\Admin;
 use Phwoolcon\Auth\Adapter\Exception;
 use Phwoolcon\Auth\AdapterInterface;
-use Phwoolcon\Auth\Auth as PhwoolconAuth;
 use Phwoolcon\Config;
 
 /**
  * Class Admin
  * @package Phwoolcon\Admin
- *
- * @property Generic $instance
- * @method static false|Admin getUser
  */
-class Auth extends PhwoolconAuth
+class Auth
 {
+    /**
+     * @var Di
+     */
+    protected static $di;
+    protected static $config = [];
+    /**
+     * @var AdapterInterface|Generic
+     */
+    protected static $instance;
+
+    public static function getInstance()
+    {
+        static::$instance or static::$instance = static::$di->getShared('admin');
+        return static::$instance;
+    }
+
+    public static function getOption($key)
+    {
+        static::$instance or static::$instance = static::$di->getShared('admin');
+        return static::$instance->getOption($key);
+    }
+
+    /**
+     * @return false|Admin
+     */
+    public static function getUser()
+    {
+        static::$instance or static::$instance = static::$di->getShared('admin');
+        return static::$instance->getUser();
+    }
 
     public static function register(Di $di)
     {
@@ -41,11 +67,10 @@ class Auth extends PhwoolconAuth
             $hasher = static::$di->getShared('security');
             $hasher->setDefaultHash($options['security']['default_hash']);
             $hasher->setWorkFactor($options['security']['work_factor']);
-            $adapter = new $class($options, $hasher);
+            $adapter = new $class($options, $hasher, $di);
             if (!$adapter instanceof AdapterInterface) {
                 throw new Exception('Admin auth adapter class should implement ' . AdapterInterface::class);
             }
-            $adapter->setDi($di);
             return $adapter;
         });
     }
