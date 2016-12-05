36e210bb585bd138c9b79a719b6e5fa23af2f31f
Christopher CHEN <chenshaoming@appgame.com>
2016-12-05 18:23:03 +0800

    feat(role): generate select options

--- src/Model/Acl/Role.php
+++ src/Model/Acl/Role.php
@@ -2,6 +2,7 @@
 namespace Phwoolcon\Admin\Model\Acl;
 
 use Phalcon\Acl\Role as PhalconRole;
+use Phwoolcon\Cache;
 use Phwoolcon\Model;
 
 /**
@@ -17,11 +18,32 @@ use Phwoolcon\Model;
  */
 class Role extends Model
 {
+    const CACHE_KEY_ROLE_OPTIONS = 'admin-role-options';
+
     protected $_table = 'admin_acl_roles';
     protected $_useDistributedId = false;
 
     public function toPhalconRole()
     {
         return new PhalconRole($this->name, $this->description);
+    }
+
+    protected function afterSave()
+    {
+        parent::afterSave();
+        Cache::delete(static::CACHE_KEY_ROLE_OPTIONS);
+    }
+
+    public static function getSelectOptions()
+    {
+        if (!$options = Cache::get($cacheKey = static::CACHE_KEY_ROLE_OPTIONS)) {
+            $options = [];
+            /* @var static $role */
+            foreach (static::find() as $role) {
+                $options[$role->getId()] = $role->getDescription();
+            }
+            Cache::set($cacheKey, $options);
+        }
+        return $options;
     }
 }
