ed7ff0e16394e79905987896d13d2ba0382eee5f
Christopher CHEN <chenshaoming@appgame.com>
2017-03-06 20:25:18 +0800

    feat: add relationship to OpLog

--- src/Model/OpLog.php
+++ src/Model/OpLog.php
@@ -3,10 +3,23 @@ namespace Phwoolcon\Admin\Model;
 
 use Phwoolcon\Model;
 
+/**
+ * Class OpLog
+ * @package Phwoolcon\Admin\Model
+ *
+ * @property Admin $admin
+ * @method Admin getAdmin()
+ */
 class OpLog extends Model
 {
     protected $_table = 'admin_op_log';
     protected $_useDistributedId = false;
+
+    public function initialize()
+    {
+        parent::initialize();
+        $this->hasOne('admin_id', Admin::class, 'id', ['alias' => 'admin']);
+    }
 
     public static function add($adminId, $action, $description = '', $result = '')
     {
